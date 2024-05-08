87c94d6c1c9394daa23032264868407e5bbe971f
sakshamjoshi <sakshamjoshi1975@gmail.com>
2024-05-08 16:10:01 +0530

    Removed hibernate and added HashMap

--- the-book-store-application/src/main/java/com/nagarro/service/OrderService.java
+++ the-book-store-application/src/main/java/com/nagarro/service/OrderService.java
@@ -1,44 +1,27 @@
 package com.nagarro.service;
 
-import com.nagarro.dao.BookDao;
-import com.nagarro.dao.OrderDao;
-import com.nagarro.entity.Book;
 import com.nagarro.entity.Order;
 import com.nagarro.util.InputUtil;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class OrderService {
+    private static final Map<Long, Order> orderInventory = new HashMap<>();
 
     public static void addOrder(){
         System.out.print("Enter ISBN of book you want to order: ");
-        String inputs = InputUtil.readInput();
-        String[] isbn = inputs.split(" ");
-        BookDao bookDao = new BookDao();
-        List<Book> books = new ArrayList<>();
-        try {
-            for (String eachIsbn : isbn) {
-                Book book = bookDao.getBookByIsbn(eachIsbn);
-                if (book.getQuantity() > 0) {
-                    books.add(book);
-                    book.setQuantity(book.getQuantity() - 1);
-                }
-            }
-            Order order = new Order(inputs, LocalDateTime.now());
-            order.setBooks(books);
-            OrderDao orderDao = new OrderDao();
-            orderDao.saveOrderToDatabase(order);
-            System.out.println("Order Saved Successfully in the database!!");
-        } catch (Exception e) {
-            System.out.println("ISBN not found in the database!! " + e);
-        }
+        String isbn = InputUtil.readInput();
+        Order order = new Order(1L, isbn, "PLACED", LocalDateTime.now());
+        orderInventory.put(1L, order);
+        System.out.println("Order Placed Successfully");
     }
 
     public static void listAllOrders(){
-        OrderDao orderDao = new OrderDao();
-        List<Order> orders = orderDao.getAllOrders();
+        List<Order> orders = new ArrayList<>(orderInventory.values());
         if (orders.isEmpty()) {
             System.out.println("There are no Orders in the database!");
         } else{
