b79cada0a2211810f7cfb4e9799a8367daa25791
sakshamjoshi <sakshamjoshi1975@gmail.com>
2024-05-13 17:04:57 +0530

    Improved Exception Handling code

--- the-book-store-application/src/main/java/com/nagarro/constant/Constant.java
+++ the-book-store-application/src/main/java/com/nagarro/constant/Constant.java
@@ -1,7 +1,5 @@
 package com.nagarro.constant;
 
-public class Constant {
-    public static final String ADMIN = "ADMIN";
-    public static final String USER = "USER";
-    public static final String EXIT = "EXIT";
+public enum Constant {
+	ADMIN, USER, EXIT, PLACED;
 }
