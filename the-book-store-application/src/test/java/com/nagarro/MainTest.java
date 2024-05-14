33a3f9b5b13e215e5781c451cda2d8191223feb1
kamadnayyar <kamad.nayyar@nagarro.com>
2024-05-14 12:40:36 +0530

    testing demo

--- the-book-store-application/src/test/java/com/nagarro/MainTest.java
+++ the-book-store-application/src/test/java/com/nagarro/MainTest.java
@@ -1,8 +1,8 @@
 package com.nagarro;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class MainTest {
 
